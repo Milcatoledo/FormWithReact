6127bdcf08a4eb36ce07ab89738bbaaf05979352
mika <mtoledom@unemi.edu.ec>
2025-09-20 00:42:42 -0500

    First component for the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import ContentForm from './components/contentForm'
+
 function App() {
     return (
       <div>
         <div>
-          <i class="material-icons">person_add</i>
+          <i className="material-icons">person_add</i>
         </div>
         <h1>Crear cuenta</h1>
-        <p class="subtitle">Únete a nuestra comunidad</p>
+        <p className="subtitle">Únete a nuestra comunidad</p>
+        <ContentForm />
       </div>
     )
 }
-
 export default App
---
+++ src/components/contentForm.jsx
@@ -0,0 +1,8 @@
+function ContentForm() {
+    return (
+        <form>
+            <p>Al registrarte, aceptas nuestros Términos de Servicio y Política de Privacidad</p>
+        </form>
+    )
+}
+export default ContentForm
